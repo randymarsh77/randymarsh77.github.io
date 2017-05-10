5abc34532db2bd0f26e02621f301900f7a3aa0c3
Matthew Preucil <mattpsbs@gmail.com>
2017-05-09 20:18:03 -0700

    Stage some files to deploy.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -26,7 +26,8 @@ const [owner, repo] = process.env.TRAVIS_REPO_SLUG.split('/');
 
 	// push it up
 	console.log('  Pushing...\n');
-	await execAsync('git commit -m "Deploying, :fingers-crossed:"');
+	await execAsync('git add *', { cwd: repo });
+	await execAsync('git commit -m "Deploying, :fingers-crossed:"', { cwd: repo });
 	await execAsync('git push origin master', { cwd: repo });
 
 	console.log('Finished!');
