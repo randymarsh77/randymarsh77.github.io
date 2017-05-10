9c0bb8a7a61a901a0cd5318a031d9ce311e4076a
Matthew Preucil <mattpsbs@gmail.com>
2017-05-09 20:11:57 -0700

    Add a rudimentary deploy script from Travis that will probably fail due to missing credentials.

---
+++ scripts/deploy.js
@@ -0,0 +1,33 @@
+import { exec } from 'child_process';
+
+function execAsync(command, options) {
+	const promise = new Promise((resolve, reject) => {
+		exec(`${command}`, options, (error, stdout, stderr) => {
+			if (error) reject(stderr);
+			resolve(stdout);
+		});
+	});
+	return promise;
+}
+
+const [owner, repo] = process.env.TRAVIS_REPO_SLUG.split('/');
+
+(async () => {
+	const dir = await execAsync('pwd');
+	console.log(`Deploying from ${dir}...`);
+
+	// clone master branch
+	console.log('  Cloning...\n');
+	await execAsync(`git clone https://github.com/${owner}/${repo}`);
+
+	// copy dist into the result, overwriting everything
+	console.log('  Copying files...\n');
+	await execAsync(`cp -r dist/ ${repo}/`);
+
+	// push it up
+	console.log('  Pushing...\n');
+	await execAsync('git commit -m "Deploying, :fingers-crossed:"');
+	await execAsync('git push origin master', { cwd: repo });
+
+	console.log('Finished!');
+})();
